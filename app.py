92c947225b87be96d11704b6ba1c41ff486a627a
sofibeta <sofia.betancurmartinez@gmail.com>
2026-03-18 18:16:02 -0500

    sentimientos

---
+++ app.py
@@ -0,0 +1,41 @@
+from textblob import TextBlob
+import pandas as pd
+import streamlit as st
+from PIL import Image
+from googletrans import Translator
+
+st.title('Análisis de Sentimiento')
+image = Image.open('emoticones.jpg')
+st.image(image)
+st.subheader("Por favor escribe en el campo de texto la frase que deseas analizar")
+
+translator = Translator()
+
+with st.sidebar:
+               st.subheader("Polaridad y Subjetividad")
+               ("""
+                Polaridad: Indica si el sentimiento expresado en el texto es positivo, negativo o neutral. 
+                Su valor oscila entre -1 (muy negativo) y 1 (muy positivo), con 0 representando un sentimiento neutral.
+                
+               Subjetividad: Mide cuánto del contenido es subjetivo (opiniones, emociones, creencias) frente a objetivo
+               (hechos). Va de 0 a 1, donde 0 es completamente objetivo y 1 es completamente subjetivo.
+
+                 """
+               ) 
+
+with st.expander('Analizar texto'):
+    text = st.text_input('Escribe por favor: ')
+    if text:
+
+        translation = translator.translate(text, src="es", dest="en")
+        trans_text = translation.text
+        blob = TextBlob(trans_text)
+        st.write('Polarity: ', round(blob.sentiment.polarity,2))
+        st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
+        x=round(blob.sentiment.polarity,2)
+        if x > 0.0 and x <=1.0:
+            st.write( 'Es un sentimiento Positivo 😊')
+        elif x >=-1 and x <= 0:
+            st.write( 'Es un sentimiento Negativo 😔')
+        else:
+            st.write( 'Es un sentimiento Neutral 😐')
