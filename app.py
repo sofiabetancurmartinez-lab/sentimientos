11c2d28496533b1e0e99abbe04a3deb42b03ee52
sofibeta <sofia.betancurmartinez@gmail.com>
2026-03-29 14:40:59 -0500

    este

--- app.py
+++ app.py
@@ -2,23 +2,17 @@ from textblob import TextBlob
 import pandas as pd
 import streamlit as st
 from PIL import Image
-from deep_translator import GoogleTranslator
-import streamlit.components.v1 as components
+from googletrans import Translator
+from streamlit_lottie import st_lottie
+import json
+
 
 st.title('Análisis de Sentimiento')
-
-# Lottie animation
-lottie_html = """
-<script src="https://unpkg.com/@lottiefiles/dotlottie-wc@0.9.3/dist/dotlottie-wc.js" type="module"></script>
-<dotlottie-wc src="https://lottie.host/3d7cd9d4-c409-4dcc-8598-2605ccdfadea/h4oQM8hK4O.lottie" style="width: 300px;height: 300px" autoplay loop></dotlottie-wc>
-"""
-components.html(lottie_html, height=350)
-
 image = Image.open('emoticones.jpg')
 st.image(image)
 st.subheader("Por favor escribe en el campo de texto la frase que deseas analizar")
 
-translator = GoogleTranslator(source='es', target='en')
+translator = Translator()
 
 with st.sidebar:
                st.subheader("Polaridad y Subjetividad")
@@ -36,20 +30,24 @@ with st.expander('Analizar texto'):
     text = st.text_input('Escribe por favor: ')
     if text:
 
-        trans_text = translator.translate(text)
+        translation = translator.translate(text, src="es", dest="en")
+        trans_text = translation.text
         blob = TextBlob(trans_text)
         st.write('Polarity: ', round(blob.sentiment.polarity,2))
         st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
         x=round(blob.sentiment.polarity,2)
         if x > 0.0 and x <=1.0:
             st.write( 'Es un sentimiento Positivo 😊')
-        elif x >=-1 and x <= 0:
+            with open ("Cute Doggie.json") as source:
+                 animation=json.load(source)
+            st.lottie(animation,width=350)
+        elif x >=-1 and x < 0:
             st.write( 'Es un sentimiento Negativo 😔')
+            with open ("Sad Emoji.json") as source:
+                 animation=json.load(source)
+            st.lottie(animation,width=350)
         else:
             st.write( 'Es un sentimiento Neutral 😐')
-
-lottie_html = """
-<script src="https://unpkg.com/@lottiefiles/dotlottie-wc@0.9.3/dist/dotlottie-wc.js" type="module"></script>
-<dotlottie-wc src="https://lottie.host/3aa56635-c331-4118-8f05-96256fe71008/5UZl7vAMBu.lottie" style="width: 300px;height: 300px" autoplay loop></dotlottie-wc>
-"""
-components.html(lottie_html, height=350)
+            with open ("Bad Cat.json") as source:
+                 animation=json.load(source)
+            st.lottie(animation,width=350)
